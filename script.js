2250606d3ba11d578d712563696b43e47daf1942
konrad <konrad.ascher@yahoo.de>
2013-08-20 01:06:22 +0200

    accordion layout trial

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ $(document).ready( function(){
 	$("#master").click( function(){
 		$(this).html("hallo");
 	});
+	$(function() {
+		$( "#pink" ).accordion({ header: "h4", collapsible: "true"});
+		$( "#MichaelBuble" ).accordion({ header: "h4", collapsible: "true"});
+	});
+
 });
